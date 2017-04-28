0fb413299f196fa53dd5551e5887d4f04160fc24
Nick Saric <nicksaric@gmail.com>
2017-04-27 18:03:38 -0700

    Added a linear search version to go alongside the counting sort version. Additional comments included.

--- src/zipcoderestrictions/ZipCodeRestrictions.java
+++ src/zipcoderestrictions/ZipCodeRestrictions.java
@@ -6,6 +6,7 @@ package zipcoderestrictions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ZipCodeRestrictions {
 
@@ -17,7 +18,7 @@ public class ZipCodeRestrictions {
      * @param reset: Whether or not to use a new list of restricted zip codes
      * or to build off of the previous one.
      */
-    public static boolean sendPackage(int zipcode, List<int[]> restricted, boolean reset) {
+    public static boolean countingSortStyle(int zipcode, List<int[]> restricted, boolean reset) {
         if (reset) {
             zipcodeList = new int[100000];
         }
@@ -31,36 +32,78 @@ public class ZipCodeRestrictions {
         return zipcodeList[zipcode] == 0; //True if sendable, false if restricted.
     }
     
-    public static void test(List<int[]> restricted, boolean reset) {
-        System.out.println("sent1: " + sendPackage(94030, restricted, reset));
+    public static boolean linearSearchStyle(int zipcode, List<int[]> restricted) {
+        ListIterator<int[]> iterator = restricted.listIterator();
+        
+        while (iterator.hasNext()) {
+            int[] range = iterator.next();
+            if (zipcode >= range[0] && zipcode <= range[1]) {
+                return false; // Within a restricted range.
+            }
+        }
+        return true; // No applicable ranges.
+    }
+    
+    public static void countingSortTest(List<int[]> restricted, boolean reset) {
+        System.out.println("countingSort1: " + countingSortStyle(94030, restricted, reset));
 
-        System.out.println("sent2: " + sendPackage(94051, restricted, reset));
+        System.out.println("countingSort2: " + countingSortStyle(94051, restricted, reset));
 
-        System.out.println("sent3: " + sendPackage(94042, restricted, reset));
+        System.out.println("countingSort3: " + countingSortStyle(94042, restricted, reset));
 
-        System.out.println("sent4: " + sendPackage(64117, restricted, reset));
+        System.out.println("countingSort4: " + countingSortStyle(64117, restricted, reset));
 
-        System.out.println("sent5: " + sendPackage(99999, restricted, reset));
+        System.out.println("countingSort5: " + countingSortStyle(99999, restricted, reset));
+    }
+    
+    public static void linearSearchTest(List<int[]> restricted) {
+        System.out.println("linearSearch1: " + linearSearchStyle(94030, restricted));
+
+        System.out.println("linearSearch2: " + linearSearchStyle(94051, restricted));
+
+        System.out.println("linearSearch3: " + linearSearchStyle(94042, restricted));
+
+        System.out.println("linearSearch4: " + linearSearchStyle(64117, restricted));
+
+        System.out.println("linearSearch5: " + linearSearchStyle(99999, restricted));
     }
 
     public static void main(String[] args) {
-        List<int[]> restricted = new ArrayList<>();
-        restricted.add(new int[]{94030, 94051});
+        // Begin Counting Sort Version tests.
+        List<int[]> countingRestricted = new ArrayList<>();
+        countingRestricted.add(new int[]{94030, 94051});
         
-        System.out.println("Test1...");
-        test(restricted, false); // Initial Test.
+        System.out.println("Counting Test1...");
+        countingSortTest(countingRestricted, false); // Initial Test.
         
-        restricted.add(new int[]{95722, 99999}); //99999 should now be false.
-        restricted.add(new int[]{30210, 80210}); //64117 should now be false.
-        restricted.add(new int[]{13579, 24680}); //No changes.
+        countingRestricted.add(new int[]{95722, 99999}); //99999 should now be false.
+        countingRestricted.add(new int[]{30210, 80210}); //64117 should now be false.
+        countingRestricted.add(new int[]{13579, 24680}); //No changes.
 
-        System.out.println("Test2...");
-        test(restricted, false); // Additional zip codes added.
+        System.out.println("Counting Test2...");
+        countingSortTest(countingRestricted, false); // Additional zip codes added.
 
-        List<int[]> newRestricted = new ArrayList<>();
-        newRestricted.add(new int[]{94030, 94051});
+        List<int[]> resetRestricted = new ArrayList<>();
+        resetRestricted.add(new int[]{94030, 94051});
         
-        System.out.println("Test3...");
-        test(newRestricted, true); // Reset restricted zip codes.
+        System.out.println("Counting Test3...");
+        countingSortTest(resetRestricted, true); // Reset restricted zip codes.
+        // End of Counting Sort Version tests.
+        
+        
+        // Begin Linear Search Version tests.
+        List<int[]> linearRestricted = new ArrayList<>();
+        linearRestricted.add(new int[]{94030, 94051});
+        
+        System.out.println("Linear Test1...");
+        linearSearchTest(linearRestricted); // Initial Test.
+        
+        linearRestricted.add(new int[]{95722, 99999}); //99999 should now be false.
+        linearRestricted.add(new int[]{30210, 80210}); //64117 should now be false.
+        linearRestricted.add(new int[]{13579, 24680}); //No changes.
+
+        System.out.println("Linear Test2...");
+        linearSearchTest(linearRestricted); // Additional zip codes added.
+        // End of Linear Search Version tests.
     }
 }
