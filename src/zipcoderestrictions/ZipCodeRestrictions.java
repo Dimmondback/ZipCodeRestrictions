d01ba804dc85ddb9ed914a3209cb51834c9eb4cd
Nick Saric <nicksaric@gmail.com>
2017-04-29 11:23:08 -0700

    Proper answer format of giving ranges of restricted zipcodes included in new method. Old testing methods left if desired.

--- src/zipcoderestrictions/ZipCodeRestrictions.java
+++ src/zipcoderestrictions/ZipCodeRestrictions.java
@@ -13,11 +13,6 @@ public class ZipCodeRestrictions {
     //Total list of zip codes.
     public static int[] zipcodeList = new int[100000]; //[0, 99999]
 
-    /*
-     * @param zipcodes: The int[] list of restricted zip codes.
-     * @param reset: Whether or not to use a new list of restricted zip codes
-     * or to build off of the previous one.
-     */
     public static boolean countingSortStyle(int zipcode, List<int[]> restricted, boolean reset) {
         if (reset) {
             zipcodeList = new int[100000];
@@ -30,6 +25,41 @@ public class ZipCodeRestrictions {
         }
 
         return zipcodeList[zipcode] == 0; //True if sendable, false if restricted.
+    }
+    
+    /*
+     * @param restricted: The list of int[] restricted zip codes.
+     * @param reset: Whether or not to use a new list of restricted zip codes
+     * or to build off of the previous one.
+     */
+    public static List<int[]> restrictedZipCodes(List<int[]> restricted, boolean reset) {
+        if (reset) {
+            zipcodeList = new int[100000];
+        }
+
+        for (int[] current : restricted) {
+            for (int i = current[0]; i <= current[1]; i++) { // Treat it like counting-sort and mark places that can't be sent to.
+                zipcodeList[i] = 1;
+            }
+        }
+        
+        int lowerbound = -1;
+        List<int[]> restrictedZipCodes = new ArrayList<>();
+        for(int i = 0; i < zipcodeList.length; i++) {
+            if (zipcodeList[i] == 0 && lowerbound != -1) {
+                restrictedZipCodes.add(new int[]{lowerbound, i-1});
+                lowerbound = -1;
+            } else if (zipcodeList[i] == 1 && lowerbound == -1) {
+                lowerbound = i;
+            }
+        }
+        
+        for(int i = 0; i < restrictedZipCodes.size(); i++) {
+            System.out.print(restrictedZipCodes.get(i)[0] + ", ");
+            System.out.println(restrictedZipCodes.get(i)[1]);
+        }
+
+        return restrictedZipCodes;
     }
     
     public static boolean linearSearchStyle(int zipcode, List<int[]> restricted) {
@@ -70,40 +100,12 @@ public class ZipCodeRestrictions {
 
     public static void main(String[] args) {
         // Begin Counting Sort Version tests.
-        List<int[]> countingRestricted = new ArrayList<>();
-        countingRestricted.add(new int[]{94030, 94051});
-        
-        System.out.println("Counting Test1...");
-        countingSortTest(countingRestricted, false); // Initial Test.
-        
-        countingRestricted.add(new int[]{95722, 99999}); //99999 should now be false.
-        countingRestricted.add(new int[]{30210, 80210}); //64117 should now be false.
-        countingRestricted.add(new int[]{13579, 24680}); //No changes.
+        List<int[]> restricted = new ArrayList<>();
+        restricted.add(new int[]{94030, 94051});
+        restricted.add(new int[]{94045, 95000});
+        restricted.add(new int[]{30210, 30210});
+        restricted.add(new int[]{13579, 24680});
 
-        System.out.println("Counting Test2...");
-        countingSortTest(countingRestricted, false); // Additional zip codes added.
-
-        List<int[]> resetRestricted = new ArrayList<>();
-        resetRestricted.add(new int[]{94030, 94051});
-        
-        System.out.println("Counting Test3...");
-        countingSortTest(resetRestricted, true); // Reset restricted zip codes.
-        // End of Counting Sort Version tests.
-        
-        
-        // Begin Linear Search Version tests.
-        List<int[]> linearRestricted = new ArrayList<>();
-        linearRestricted.add(new int[]{94030, 94051});
-        
-        System.out.println("Linear Test1...");
-        linearSearchTest(linearRestricted); // Initial Test.
-        
-        linearRestricted.add(new int[]{95722, 99999}); //99999 should now be false.
-        linearRestricted.add(new int[]{30210, 80210}); //64117 should now be false.
-        linearRestricted.add(new int[]{13579, 24680}); //No changes.
-
-        System.out.println("Linear Test2...");
-        linearSearchTest(linearRestricted); // Additional zip codes added.
-        // End of Linear Search Version tests.
+        restrictedZipCodes(restricted, false);
     }
 }
